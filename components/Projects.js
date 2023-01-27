69d63db10baca8dec21fb9ba29d1e4b011e324d8
Jon Sadowski <jwsadowski@gmail.com>
2023-01-27 14:55:52 -0600

    added projects

---
+++ components/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+        <div>
+            <h1>PROJECTS</h1>
+            <li>Movie Soundtrack finder</li>
+
+            <li><a href = "https://github.com/megsra17/movie-soundtrack-finder.git"></a></li>
+
+            <li><img src="https://github.com/megsra17/movie-soundtrack-finder/blob/main/Screen%20Shot%202022-10-18%20at%205.46.00%20PM.png"></img></li>
+
+            <li><a href = "https://github.com/megsra17/movie-soundtrack-finder/blob/main/index.html"></a></li>
+            
+        </div>
+    )
+}
--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact() {
     return (
         <div>
-            <h1>Contact Page</h1>
+            <h1>Contact</h1>
             <ul>
                 <li><a href = "mailto: https://jwsadowski@gmail.com">Send Email</a></li>
                 <li><a href = "https://github.com/jwsadowski">Jon's github page</a></li>
