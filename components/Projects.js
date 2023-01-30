d4d28564463d2cd9fec6ef9395c6641f94975cc4
Jon Sadowski <jwsadowski@gmail.com>
2023-01-30 14:23:41 -0600

    added css files to components

--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Projects() {
+function Projects() {
     return (
         <div>
             <h1>PROJECTS</h1>
+            <ul>
             <li>Movie Soundtrack finder</li>
 
             <li><a href = "https://github.com/megsra17/movie-soundtrack-finder.git"></a></li>
@@ -11,7 +12,36 @@ export default function Projects() {
             <li><img src="https://github.com/megsra17/movie-soundtrack-finder/blob/main/Screen%20Shot%202022-10-18%20at%205.46.00%20PM.png"></img></li>
 
             <li><a href = "https://github.com/megsra17/movie-soundtrack-finder/blob/main/index.html"></a></li>
-            
+            </ul>
         </div>
-    )
+
+        <div>
+            <h1>PROJECTS</h1>
+            <ul>
+            <li>We Do</li>
+
+            <li><a href = "https://github.com/ddwk21/we-do.git"></a></li>
+
+            <li><img src="https://github.com/ddwk21/we-do/blob/main/public/assets/homepage.JPG"></img></li>
+
+            <li><a href = "https://we-do-application.herokuapp.com/"></a></li>
+            </ul>
+        </div>
+
+        <div>
+           <h1>PROJECTS</h1>
+           <ul>
+           <li>Text Editor</li>
+
+           <li><a href = "https://github.com/jwsadowski/text-editor-challenge.git"></a></li>
+
+           <li><img src="https://user-images.githubusercontent.com/111463494/214950568-620ec783-cf76-449d-827a-f55f72ab9bef.png"></img></li>
+
+           <li><a href = "https://calm-brook-66511.herokuapp.com/"></a></li>
+           </ul>
+        </div>
+
+    );
 }
+
+export default Projects;
