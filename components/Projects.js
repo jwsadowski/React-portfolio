61b77dc919bf80472ca99ba4f464a9fb22355990
Jon Sadowski <jwsadowski@gmail.com>
2023-01-30 16:59:54 -0600

    redone projects file

--- components/Projects.js
+++ components/Projects.js
@@ -1,48 +1,15 @@
 import React from "react";
 
-function Projects() {
+function Projects(props) {
     return (
-        <>
-        <div>
-            <h1>PROJECTS</h1>
-            <ul>
-            <li>Movie Soundtrack finder</li>
-
-            <li><a href = "https://github.com/megsra17/movie-soundtrack-finder.git"></a></li>
-
-            <li><img src="https://github.com/megsra17/movie-soundtrack-finder/blob/main/Screen%20Shot%202022-10-18%20at%205.46.00%20PM.png"></img></li>
-
-            <li><a href = "https://github.com/megsra17/movie-soundtrack-finder/blob/main/index.html"></a></li>
-            </ul>
-        </div>
-
-        <div>
-            <h1>PROJECTS</h1>
-            <ul>
-            <li>We Do</li>
-
-            <li><a href = "https://github.com/ddwk21/we-do.git"></a></li>
-
-            <li><img src="https://github.com/ddwk21/we-do/blob/main/public/assets/homepage.JPG"></img></li>
-
-            <li><a href = "https://we-do-application.herokuapp.com/"></a></li>
-            </ul>
-        </div>
-
-        <div>
-           <h1>PROJECTS</h1>
-           <ul>
-           <li>Text Editor</li>
-
-           <li><a href = "https://github.com/jwsadowski/text-editor-challenge.git"></a></li>
-
-           <li><img src="https://user-images.githubusercontent.com/111463494/214950568-620ec783-cf76-449d-827a-f55f72ab9bef.png"></img></li>
-
-           <li><a href = "https://calm-brook-66511.herokuapp.com/"></a></li>
-           </ul>
-        </div>
-        </>
+        <ul className="list-group">
+            {props.Projects.map(item => (
+                <li className="list-group-item" key={item.id}>
+                    {item.name}
+                </li>
+            ))}
+        </ul>
     );
-}
+}        
 
 export default Projects;
